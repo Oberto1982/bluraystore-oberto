45b7ef89bc684c58c9a3b41df5504df2f6741bd3
oberto52 <diego_oberto@yahoo.com.ar>
2021-03-11 23:07:51 -0300

    Navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links' 
+    },
+
+    {
+        title: '3D + Blu-Ray',
+        url: '#',
+        cName: 'nav-links' 
+    },
+
+    {
+        title: '4k UHD',
+        url: '#',
+        cName: 'nav-links' 
+    },
+
+    {
+        title: 'Vinilos',
+        url: '#',
+        cName: 'nav-links' 
+    },
+
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links' 
+    },
+
+]
